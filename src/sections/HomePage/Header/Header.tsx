05f24b36a456679e56a4183bf33dee0d20c59b9b
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-12-10 12:05:36 +0100

    Add header

---
+++ src/sections/HomePage/Header/Header.tsx
@@ -0,0 +1,52 @@
+// Libraries
+import React from 'react';
+import {Link} from 'gatsby';
+import {GatsbyImage} from 'gatsby-plugin-image';
+// Components
+import Container from '@components/Container/Container';
+import {Title, Text} from '@components/typography';
+import Button from '@components/Button/Button';
+// Hooks
+import useImage from '@hooks/useImage';
+// Types
+import {Tag} from '@customTypes/tags';
+import {TitleTag, TitleVariant} from '@components/typography/Title/Title';
+import {FontColors} from '@customTypes/main';
+// Styles
+import * as styles from './Header.module.scss';
+
+const Header = () => {
+	const {hand} = useImage();
+
+	return (
+		<Container as={Tag.Header} classes={styles.header}>
+			<div className={styles.headerImage}>
+				{hand && <GatsbyImage image={hand} alt='Ręka' />}
+			</div>
+			<div className={styles.headerContent}>
+				<Title
+					tag={TitleTag.h1}
+					variant={TitleVariant.main}
+					color={FontColors.light} classes={styles.headerTitle}>
+					<span data-color={FontColors.yellow}>Sieć doświadczonych ekspertów</span> i
+					zawodowo spełnionych biznesowych mentorów na żądanie.
+				</Title>
+				<Text color={FontColors.light}>
+					Niezależnie od tego, czy współpracujesz z konkretnym ekspertem, czy szukasz
+					informacji od wielu mentorów, Twoja sieć natychmiast staje się silniejsza.
+					Wszyscy eksperci Skillguru są sprawdzani i mają udokumentowane
+					doświadczenie jako odnoszący sukcesy eksperci i profesjonaliści w swoich
+					branżach.
+				</Text>
+				<div className={styles.headerCta}>
+					<Button>Znajdź Mentorów</Button>
+					<Link className={styles.headerLink} to='/'>
+						Czytaj więcej
+					</Link>
+				</div>
+			</div>
+		</Container>
+	);
+};
+
+export default Header;
